5303f323d8808adba1ea3b41e26a406ab587052b
Piroska Werner <piroska.werner@gmail.com>
2021-07-23 15:43:58 -0500

    feat: Exports app from index.ts and creates indexSpec.ts

---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,20 @@
+import { agent as request } from 'supertest';
+import app from '../index';
+
+//test api/sampleRoute endpoint
+describe('GET api/sampleRoute', () => {
+  it('responds with: Application starting page', (done) => {
+    request(app)
+      .get('/api/sampleRoute')
+      .expect(200)
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .then((response) => {
+        expect(response.text).toBe('Application starting page');
+        done();
+      })
+      .catch((Error) => {
+        Error ? done.fail(Error) : done();
+      });
+  });
+});
+
