65b624a72e40f62b3c816556313315b39f42ecdb
Piroska Werner <piroska.werner@gmail.com>
2021-07-25 19:05:41 -0500

    feat: Displays picture and its dimensions on page

--- src/routes/api/sampleRoute.ts
+++ src/routes/api/sampleRoute.ts
@@ -6,7 +6,7 @@ const sampleRoute = express.Router();
 
 //set up route
 sampleRoute.get('/', processor, (req, res) => {
-  res.send('Application starting page');
+  // res.send('Application starting page');
 });
 
 //export route
--- src/routes/utilities/imageProcessor.ts
+++ src/routes/utilities/imageProcessor.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 
 const processor = (req: express.Request, res: express.Response): void => {
-	const fileName = req.query.file;
+	const fileName  = req.query.file;
 	const width = req.query.width;
 	const height = req.query.height;
 	console.log(`file name: ${fileName}\nwidth: ${width}\nheight: ${height}`);
+	res.send(`Create a ${width} x ${height} picture from ${fileName}`);
 }
 
 export default processor;
