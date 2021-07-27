aba5585e7c70521470379d5dab183f10de4af163
Piroska Werner <piroska.werner@gmail.com>
2021-07-27 18:46:52 -0500

    feat: Server sends back image file requested

--- src/routes/utilities/imageProcessor.ts
+++ src/routes/utilities/imageProcessor.ts
@@ -1,11 +1,20 @@
 import express from 'express'
+import path from 'path';
 
 const processor = (req: express.Request, res: express.Response): void => {
-	const fileName  = req.query.file;
+	const fileName = req.query.file;
 	const width = req.query.width;
 	const height = req.query.height;
 	console.log(`file name: ${fileName}\nwidth: ${width}\nheight: ${height}`);
-	res.send(`Create a ${width} x ${height} picture from ${fileName}`);
+	const options = {
+		root: path.join(__dirname, '../assets/thumbs/')
+	}
+	// res.sendFile(`${fileName as string}_${width}-${height}.jpg`, options, (err) => {
+	res.sendFile(`${fileName}_${width}x${height}.jpg`, options, (err) => {
+		if(err) {
+			console.log(err);
+		}
+	})
 }
 
 export default processor;
