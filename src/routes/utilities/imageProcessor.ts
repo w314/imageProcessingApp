4788419734d5ff419d54ac270b34c7f0db812ed5
Piroska Werner <piroska.werner@gmail.com>
2021-07-28 15:31:23 -0500

    feat: Checks if resized image exits and if not checks if original image exists.

--- src/routes/utilities/imageProcessor.ts
+++ src/routes/utilities/imageProcessor.ts
@@ -1,18 +1,30 @@
 import express from 'express'
 import path from 'path';
+import { promises as fs } from 'fs';
 
 const processor = (req: express.Request, res: express.Response): void => {
 	const fileName = req.query.file;
 	const width = req.query.width;
 	const height = req.query.height;
-	console.log(`file name: ${fileName}\nwidth: ${width}\nheight: ${height}`);
 	const options = {
 		root: path.join(__dirname, '../assets/thumbs/')
 	}
-	// res.sendFile(`${fileName as string}_${width}-${height}.jpg`, options, (err) => {
-	res.sendFile(`${fileName}_${width}x${height}.jpg`, options, (err) => {
+	// return requested file if exist with the required dimensions
+	res.sendFile(`${fileName}_${width}x${height}.jpg`, options, async (err) => {
 		if(err) {
-			console.log(err);
+			console.log(`${fileName}.jpg with width: ${width} and height: ${height} doesn't exists.`);
+			// check if file requested exist under images folder
+			try {
+				await fs.access(path.resolve(__dirname, '../assets/images/',`${fileName}.jpg`));
+				console.log(`File name ${fileName}.jpg available to resize.`);
+			}
+			catch (err) {
+				// console.log(err);
+				console.log(`There is no such file as ${fileName}`);
+			}
+		}
+		else {
+			console.log(`Returning ${fileName}.jpg, width: ${width}, height: ${height}`);
 		}
 	})
 }
