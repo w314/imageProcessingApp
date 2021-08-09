c735f8f9afd872ed195fb2e74b434243d1f91701
Piroska Werner <piroska.werner@gmail.com>
2021-08-09 14:10:06 -0500

    feat: Sends original message if no height and width given

--- src/routes/utilities/imageProcessor.ts
+++ src/routes/utilities/imageProcessor.ts
@@ -1,20 +1,38 @@
 import express from 'express'
 import path from 'path';
 import { promises as fs } from 'fs';
+import { exit } from 'process';
 
 const processor = (req: express.Request, res: express.Response): void => {
 	const fileName = req.query.file;
 	const width = req.query.width;
 	const height = req.query.height;
-	const options = {
+	const optionsThumbs = {
 		root: path.join(__dirname, '../assets/thumbs/')
 	}
+	const optionsImages = {
+		root: path.join(__dirname, '../assets/images/')
+	}
 	// return requested file if exist with the required dimensions
+	console.log(`\n${fileName}.jpg ${width} x ${height} is requested`);
 	if(fileName == undefined) {
 		// throw Error('No file name given.');
 		res.send('Cannot process picture, no file parameter in url.');
 	}
-	res.sendFile(`${fileName}_${width}x${height}.jpg`, options, async (err) => {
+
+	if(width == undefined && height == undefined) {
+		console.log(`No width and height parameters are given, returning original ${fileName}.jpg`);
+		res.sendFile(`${fileName}.jpg`, optionsImages, async (err) => {
+			if (err) {
+				console.log(err);
+			}
+		})
+		return;
+	}
+
+	
+
+	res.sendFile(`${fileName}_${width}x${height}.jpg`, optionsThumbs, async (err) => {
 		if(err) {
 			console.log(`${fileName}.jpg with width: ${width} and height: ${height} doesn't exists.`);
 			// check if file requested exist under images folder
