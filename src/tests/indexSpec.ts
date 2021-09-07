9c27a5ee4e14eb5f7cf1e49783ac5e246fce03a5
Piroska Werner <piroska.werner@gmail.com>
2021-09-07 11:16:53 +0200

    test: Tests if image file name is valid

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -9,26 +9,26 @@ const height: string = '400';
 
 //test api/sampleRoute endpoint with no file parameter
 describe('Checking endpoint', () => {
-  it('responds with "Cannot process picture, no file parameter in url." when file parameter is missing in  url', (done) => {
+  it('responds with "Cannot process request, no file parameter in url." when file parameter is missing in  url', (done) => {
     request(app)
       .get('/api/sampleRoute?width=600&height=400')
       .expect(200)
       .expect('Content-Type', 'text/html; charset=utf-8')
       .then((response) => {
-        expect(response.text).toBe('Cannot process picture, no file parameter in url.');
+        expect(response.text).toBe('Cannot process request, no file parameter in url.');
         done();
       })
       .catch((Error) => {
         Error ? done.fail(Error) : done();
       });
   });
-  it('responds with "Invalid image file." when non-existent file name is given', (done) => {
+  it('responds with "Cannot process request, image nonexistentImage.jpg does not exist." when non-existent file name is given', (done) => {
     request(app)
       .get('/api/sampleRoute?file=nonexistentImage')
       .expect(200)
       .expect('Content-Type', 'text/html; charset=utf-8')
       .then(response => {
-        expect(response.text).toBe('Invalid image file.');
+        expect(response.text).toBe('Cannot process request, image nonexistentImage.jpg does not exist.');
         done();
       })
       .catch(Error => {
