dffd49638b933fd34afd7fb45c0a856ed50241b2
Piroska Werner <piroska.werner@gmail.com>
2021-10-19 16:07:10 -0500

    test: Refaktor using variables in urlStrings

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -2,24 +2,22 @@ import { agent as request } from 'supertest';
 import app from '../index';
 import { promises as fs } from 'fs';
 import path from 'path'
-// import { doesNotMatch, doesNotReject, rejects } from 'assert';
-// import { resolve } from 'path/posix';
 
 
-// const thumbFile: string = `${image}_${width}x${height}`;
 const thumbsDir: string = path.join('.', 'assets', 'thumbs');
-const image: string = 'fjord';
-const width: string = '600';
-const height: string = '400';
-// const optionsThumbs = {
-//   root: path.join(__dirname, thumbsDir)
-// }
+const endPoint: string = '/api/images'
+const filePath = (fileName: string, width: string, height: string) => {
+  return ''
+}
+const urlString = (fileName?: string, width?: string, height?: string) => {
+  return `${endPoint}?file=${fileName}&width=${width}&height=${height}`
+}
 
 // tests api/images endpoint with no file parameter
 describe('Checking API/images endpoint', () => {
   it('handles missing file parameter', (done) => {
     request(app)
-      .get('/api/images?width=600&height=400')
+      .get(endPoint)
       .expect(400)
       .expect('Content-Type', 'text/html; charset=utf-8')
       .then((response) => {
@@ -30,13 +28,12 @@ describe('Checking API/images endpoint', () => {
         Error ? done.fail(Error) : done();
       });
   });
+
   // tests api/images endpoint with invalid file parameter
   it('handles invalid file name', (done) => {
-
     const fileName: string = 'nonexistentImage';
-
     request(app)
-      .get(`/api/images?file=nonexistentImage`)
+      .get(urlString(fileName))
       .expect(404)
       .expect('Content-Type', 'text/html; charset=utf-8')
       .then(response => {
@@ -50,8 +47,10 @@ describe('Checking API/images endpoint', () => {
 
   // tests api/images to send original image file if no width and no height parameters are given
   it('sends original image file if no width and no height parameters are given in url', (done) => {
+    const fileName = 'fjord'
+    const urlString: string = `${endPoint}?file=${fileName}`
     request(app)
-      .get('/api/images?file=fjord')
+      .get(urlString)
       .expect(200)
       .end(function(err, res) {
         if (err) {
@@ -64,8 +63,10 @@ describe('Checking API/images endpoint', () => {
 
   // tests creating requested new image
   it('creates requested image with requested width and height', async (done) => {
-
-    const testFile = path.join(`${thumbsDir}`, `${image}_${width}x${height}.jpg`);
+    const fileName: string = 'fjord';
+    const width: string = '600';
+    const height: string = '400';    
+    const testFile = path.join(`${thumbsDir}`, `${fileName}_${width}x${height}.jpg`);
 
     // set-up delete testFile if necessary
     try {
@@ -86,8 +87,7 @@ describe('Checking API/images endpoint', () => {
     }
 
     await request(app)
-      // .get(`/api/image?file=${image}&width=${width}&height=${height}`)
-      .get('/api/images?file=fjord&width=600&height=400')
+      .get(urlString(fileName, width, height))
       .expect(200)
       .expect('Content-Length', '41523')
     
@@ -103,13 +103,13 @@ describe('Checking API/images endpoint', () => {
 
   // test requesting already created thumb image    
   it('serves image from under thumbs directory if it exists', async () => {
-
-    const existingThumbName: string = 'palmtunnel_300x200.jpg'
-    const existingThumbPath: string = path.join(`${thumbsDir}`, `${existingThumbName}`);
-
+    const fileName: string = 'palmtunnel';
+    const width: string = '300';
+    const height: string = '200';    
+    const testFile = path.join(`${thumbsDir}`, `${fileName}_${width}x${height}.jpg`);
 
     // check if file to be requested already exists
-    await expectAsync(fs.stat(existingThumbPath)).toBeResolved()
+    await expectAsync(fs.stat(testFile)).toBeResolved()
     console.log(`\nTEST: file to be requested already exists under thumbs directory`)
 
     // count number of files under thumbs directory before making request
@@ -118,7 +118,7 @@ describe('Checking API/images endpoint', () => {
     
     //request image
     await request(app)
-      .get('/api/images?file=palmtunnel&width=300&height=200')
+      .get(urlString(fileName, width, height))
       .expect(200)
 
     // check that no new files were created during request
